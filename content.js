35d80e8553e93e808a8b680e0290ce9fe067652a
Akram Aznakour <35656299+akramaznakour@users.noreply.github.com>
2023-06-26 19:43:06 +0100

    Added buttonManager

--- content.js
+++ content.js
@@ -1,66 +1,92 @@
-// Function to check if the current URL matches the pattern
-function checkURLPattern() {
-  const urlPattern = /^https:\/\/github\.com\/[\w-]+\/[\w-]+\/pull\/\d+\/files/;
-  return urlPattern.test(window.location.href);
-}
-
-// Function to add a button and bind the click event
-function addButton(text, inputSelector) {
-  const button = document.createElement("button");
-  button.textContent = text;
-  button.classList.add("btn-sm"); // Add a CSS class to the button element
-  button.classList.add("btn"); // Add a CSS class to the button element
-  button.style.marginLeft = "5px"; // Add some margin to the left of the button
-
-  button.addEventListener("click", function () {
-    // Script to execute when the button is clicked
-    [...document.querySelectorAll(inputSelector)].forEach((a) => a.click());
-  });
-
-  // Add the button to the page
-  const detailsContainer = document.querySelector(
-    "#files_bucket > diff-file-filter > diff-layout > div.pr-toolbar.js-sticky.js-position-sticky.d-flex > div > div.flex-grow-0.flex-shrink-0.pr-review-tools"
-  );
-
-  if (detailsContainer) {
-    detailsContainer.appendChild(button);
+class URLPatternChecker {
+  static checkURLPattern(url, pattern) {
+    const urlPattern = new RegExp(pattern);
+    return urlPattern.test(url);
   }
 }
 
-// Function to add buttons for active and inactive inputs
-function addButtonForInputs(active, inactive) {
-  if (checkURLPattern()) {
-    addButton(active.text, active.selector);
-    addButton(inactive.text, inactive.selector);
+class Button {
+  constructor(text, onClick, parentElementSelector, pattern) {
+    this.text = text;
+    this.onClick = onClick;
+    this.pattern = pattern;
+    this.parentElementSelector = parentElementSelector;
+    this.button = document.createElement("button");
+    this.button.textContent = this.text;
+    this.button.classList.add("btn-sm", "btn");
+    this.button.style.marginLeft = "5px";
+    this.button.addEventListener("click", this.handleButtonClick.bind(this));
   }
-}
 
-// Constants for selectors
-const activeInputsSelector =
-  "div div.js-replace-file-header-review.d-flex > form > label > input:not([checked])";
-const inactiveInputsSelector =
-  "div div.js-replace-file-header-review.d-flex > form > label > input[checked]";
+  handleButtonClick() {
+    this.onClick();
+  }
 
-// Function to add buttons for active and inactive inputs with default text and selectors
-function addDefaultButtonsForInputs() {
-  addButtonForInputs(
-    {
-      text: "Mark all as viewed",
-      selector: activeInputsSelector,
-    },
-    {
-      text: "Mark all as unviewed",
-      selector: inactiveInputsSelector,
+  appendToContainer() {
+    const parentElement = document.querySelector(this.parentElementSelector);
+    if (parentElement) {
+      parentElement.appendChild(this.button);
     }
-  );
+  }
 }
 
-// Check URL pattern on initial page load
-addDefaultButtonsForInputs();
+class ButtonManager {
+  constructor(buttons) {
+    this.buttons = buttons.map((button) => {
+      const { text, onClick, parentElementSelector, pattern } = button;
+      return new Button(text, onClick, parentElementSelector, pattern);
+    });
+  }
 
-// Check URL pattern on page change
-// turbo:render event is fired when the page is changed in GitHub
-// because GitHub uses Turbo (https://turbo.hotwire.dev/)
-window.addEventListener("turbo:render", function () {
-  addDefaultButtonsForInputs();
-});
+  addButtons() {
+    const currentURL = window.location.href;
+
+    this.buttons.forEach((button) => {
+      if (URLPatternChecker.checkURLPattern(currentURL, button.pattern)) {
+        button.appendToContainer();
+      }
+    });
+  }
+}
+
+const parentElementSelector =
+  "#files_bucket > diff-file-filter > diff-layout > div.pr-toolbar.js-sticky.js-position-sticky.d-flex > div > div.flex-grow-0.flex-shrink-0.pr-review-tools";
+
+const buttons = [
+  {
+    text: "Mark all as viewed",
+    pattern:
+      "^https:\\/\\/github\\.com\\/[\\w-]+\\/[\\w-]+\\/pull\\/\\d+\\/files",
+    onClick: function markAllAsViewed() {
+      const activeInputs = document.querySelectorAll(
+        "div div.js-replace-file-header-review.d-flex > form > label > input:not([checked])"
+      );
+      activeInputs.forEach((input) => input.click());
+    },
+    parentElementSelector: parentElementSelector,
+  },
+  {
+    text: "Mark all as unviewed",
+    pattern:
+      "^https:\\/\\/github\\.com\\/[\\w-]+\\/[\\w-]+\\/pull\\/\\d+\\/files",
+    onClick: function markAllAsUnviewed() {
+      const inactiveInputs = document.querySelectorAll(
+        "div div.js-replace-file-header-review.d-flex > form > label > input[checked]"
+      );
+      inactiveInputs.forEach((input) => input.click());
+    },
+    parentElementSelector: parentElementSelector,
+  },
+];
+
+const buttonManager = new ButtonManager(buttons);
+
+function addButtons() {
+  buttonManager.addButtons();
+}
+
+addButtons();
+
+window.addEventListener("turbo:render", addButtons);
+
+console.log("Github PR helper loaded");
